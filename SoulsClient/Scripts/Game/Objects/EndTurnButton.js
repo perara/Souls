f57c77d8eb9863052c8aaf2239ec5aea1f2acf17
Paul Lilleland <paul_r_l@hotmail.com>
2014-03-23 15:26:17 +0100

    FIXED Endturn bug making the player able to click nexturn while it is still spinning CLEANED Endturn class

--- SoulsClient/Scripts/Game/Objects/EndTurnButton.js
+++ SoulsClient/Scripts/Game/Objects/EndTurnButton.js
@@ -1,6 +1,7 @@
 ﻿define("endturnbutton", ["jquery", "asset", "pixi", "conf", "messages"], function ($, Asset, Pixi, Conf, Message) {
 
-    EndTurnButton = function (engine) {
+    EndTurnButton = function (engine)
+    {
         this.engine = engine;
         console.log("> EndTurnButton Loaded")
 
@@ -18,41 +19,52 @@
     // Constructor
     EndTurnButton.prototype.constructor = EndTurnButton;
 
-    EndTurnButton.prototype.Init = function (active) {
+    EndTurnButton.prototype.Init = function ()
+    {
         // Make endround button
         this.anchor = { x: 0.5, y: 0.5 };
         this.width = 200;
         this.height = 75;
         this.x = Conf.width - 310;
         this.y = (Conf.height / 2) + 10;
-        this.active = active;
-        this.active = true;
         this.swapTexture = undefined;
+        this.active = undefined;
+        this.interactive = false;
     }
 
-
-    EndTurnButton.prototype.SwapTexture = function () {
+    EndTurnButton.prototype.SwapTurn = function ()
+    {
+        // Swap the texture
         var tmpTexture = this.swapTexture;
         this.swapTexture = this.texture;
         this.setTexture(tmpTexture);
+
+        // Set interactivity
+        this.active = (this.active) ? false : true;
+        this.interactive = this.active;
     }
 
-    EndTurnButton.prototype.SetPlayerTurn = function (active) {
-        if (active) {
+    EndTurnButton.prototype.SetPlayerTurn = function (active)
+    {
+        if (active)
+        {
             this.setTexture(this.enabledTexture);
             this.swapTexture = this.disabledTexture;
-            this.interactive = true;
+            this.active = true;
         }
-        else {
+        else
+        {
             this.setTexture(this.disabledTexture);
             this.swapTexture = this.enabledTexture;
-            this.interactive = false;
+            this.active = false;
         }
+        this.interactive = this.active;
+        console.log(this.active);
     }
 
     // Spins the button and activates\deactivated it depending on input
-    EndTurnButton.prototype.Spin = function () {
-
+    EndTurnButton.prototype.Spin = function ()
+    {
         var originalHeight = this.height;
 
         Asset.GetSound(Asset.Sound.END_TURN).play();
@@ -60,7 +72,8 @@
         var that = this;
         var positive = this.scale.y;
         var position = { scaleY: this.scale.y }
-        this.interactive = (this.interactive) ? false : true;
+        that.interactive = false;
+        
         this.engine.CreateJS.Tween.get(position, { onChange: onChange })
             .to({ scaleY: this.scale.y * (-1) }, 100)
             .to({ scaleY: positive }, 100)
@@ -70,29 +83,28 @@
             .to({ scaleY: positive }, 100)
             .to({ scaleY: this.scale.y * (-1) }, 100)
             .to({ scaleY: positive }, 100)
-            .call(function () {
-                that.SwapTexture();
-            })
-
-        function onChange(e) {
+            .call(function ()
+            {
+                that.SwapTurn();
+            });
+        
+        function onChange(e)
+        {
             that.scale.y = position.scaleY;
         }
     }
 
     // When endturn sign is pushed and interactive
-    EndTurnButton.prototype.mousedown = EndTurnButton.prototype.touchstart = function () {
-
-        // TODO Send endturn request to server
-        // Run this if endturn response is OK
-
+    EndTurnButton.prototype.mousedown = EndTurnButton.prototype.touchstart = function ()
+    {
         this.Spin();
         this.RequestNextTurn();
     }
 
-    EndTurnButton.prototype.RequestNextTurn = function () {
+    EndTurnButton.prototype.RequestNextTurn = function ()
+    {
         var json = Message.GAME.NEXT_TURN;
         this.engine.gameSocket.send(json);
-        console.log("Sendt endturn request");
     }
 
     return EndTurnButton;
