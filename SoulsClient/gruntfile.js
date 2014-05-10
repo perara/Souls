ed7b88e778f9ac50f179a815243e58c1567a7d13
Per-Arne Andersen <perara12@gmail.com>
2014-05-10 05:23:15 +0200

    - Added grunfile

---
+++ SoulsClient/gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				// define a string to put between each file in the concatenated output
+				separator: ';'
+			},
+			dist: {
+				// the files to concatenate
+				src: ['Scripts/Game/**/*.js'],
+				// the location of the resulting JS file
+				dest: 'Scripts/<%= pkg.name %>.js'
+				}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd-ss") %> */\n'
+			},
+			build: {
+				src: 'Scripts/<%= pkg.name %>.js',
+				dest: 'Scripts/<%= pkg.name %>.min.js'
+			}
+		},
+		jshint: {
+			// define the files to lint
+			files: ['gruntfile.js', 'Scripts/Game/**/*.js'],
+			// configure JSHint (documented at http://www.jshint.com/docs/)
+			options: {
+			// more options here if you want to override JSHint defaults
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true
+				}
+			}
+		}
+	});
+
+	// Load the plugin that provides the tasks.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	//grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	// Default task(s).
+	grunt.registerTask('default', ['concat', 'uglify']);
+	
+};
